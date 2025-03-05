4b0f027ea7f2f2e8a3fd711d95aa601385888866
mhmodfrmwi <mahmoudframawi98@gmail.com>
2025-03-05 21:34:16 +0200

    create endpoints for guests using api views, CBV (class based views), mixins, generic views and viewsets

--- project/urls.py
+++ project/urls.py
@@ -15,13 +15,47 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from tickets import views
+from rest_framework.routers import DefaultRouter
+router=DefaultRouter()
+router.register('guests',views.GuestViewSet)
+
+router.register('movies',views.ModelViewSet)
+
+router.register('reservations',views.ReservationViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     #1
     path('django/jsonresponsenomodel/',views.no_rest_no_model),
     #2
-    path('django/jsonresponsefrommodel/',views.no_rest_with_model)
+    path('django/jsonresponsefrommodel/',views.no_rest_with_model),
+
+    #3
+    path('rest/fbv/',views.fbv),
+
+    #4
+    path('rest/fbv/<int:pk>',views.fbv_detail),
+
+    #5
+    path('rest/cbv/',views.CBV_List.as_view()),
+
+    #6
+    path('rest/cbv/<int:pk>',views.CBV_pk.as_view()),
+
+    #7
+    path('rest/mixin/',views.Mixin_List.as_view()),
+
+    #8
+    path('rest/mixin/<int:pk>',views.Mixin_pk.as_view()),
+
+    #9
+    path('rest/generics/',views.Generic_List.as_view()),
+
+    #10
+    path('rest/generics/<int:pk>',views.Generic_pk.as_view()),
+
+    #11
+    path('rest/viewset/',include(router.urls)),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
+
+from tickets.serializers import GuestSerializer,MovieSerializer,ReservationSerializer
 from .models import Guest,Reservation,Movie
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from django.http import Http404
 # Create your views here.
 
 # without rest and no model query
@@ -20,3 +28,128 @@ def no_rest_with_model(request):
         'guests':list(data.values('first_name','last_name','phone_number'))
     }
     return JsonResponse(response)
+
+# rest from model
+
+@api_view(['GET', 'POST'])
+def fbv(request):
+    if request.method=="GET":
+        guests=Guest.objects.all()
+        serializer=GuestSerializer(guests,many=True)
+        return Response(serializer.data)
+    elif request.method=="POST":
+        serializer=GuestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def fbv_detail(request,pk):
+    if request.method =="GET":
+        guest=Guest.objects.get(pk=pk)
+        serializer=GuestSerializer(guest)
+        return Response(serializer.data,status.HTTP_200_OK)
+    elif request.method =="PUT":
+        guest=Guest.objects.get(pk=pk)
+        serializer=GuestSerializer(guest,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method =="DELETE":
+        guest=Guest.objects.get(pk=pk)
+        guest.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+#CBV class based views
+class CBV_List(APIView):
+    def get(self,request):
+        guests=Guest.objects.all()
+        serializer=GuestSerializer(guests,many=True)
+        return Response(serializer.data)
+    
+    def post(self,request):
+        serializer=GuestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+class CBV_pk(APIView):
+    def get_object(self,pk):
+        try:
+            return Guest.objects.get(pk=pk)
+        except Guest.DoesNotExist:
+            raise Http404
+    
+    def get(self,request,pk):
+        guest=self.get_object(pk)
+        serializer=GuestSerializer(guest)
+        return Response(serializer.data)
+    
+    def put(self,request,pk):
+        guest=self.get_object(pk)
+        serializer=GuestSerializer(guest,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk):
+        guest=self.get_object(pk)
+        guest.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# mixins list
+
+from rest_framework import mixins
+from rest_framework.generics import GenericAPIView
+
+class Mixin_List(mixins.ListModelMixin,mixins.CreateModelMixin, GenericAPIView):
+    queryset=Guest.objects.all()
+    serializer_class=GuestSerializer
+    def get(self,request):
+        return self.list(request)
+    
+    def post(self,request):
+        return self.create(request)
+
+class Mixin_pk(mixins.RetrieveModelMixin,mixins.UpdateModelMixin, mixins.DestroyModelMixin, GenericAPIView):
+    queryset=Guest.objects.all()
+    serializer_class=GuestSerializer
+    def get(self,request,pk):
+        return self.retrieve(request,pk)
+    
+    def put(self,request,pk):
+        return self.update(request,pk)
+    
+    def delete(self,request,pk):
+        return self.destroy(request,pk)
+
+# generic views
+
+from rest_framework import generics
+
+class Generic_List(generics.ListCreateAPIView):
+    queryset=Guest.objects.all()
+    serializer_class=GuestSerializer
+
+class Generic_pk(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Guest.objects.all()
+    serializer_class=GuestSerializer
+
+# viewsets
+
+from rest_framework import viewsets
+
+class GuestViewSet(viewsets.ModelViewSet):
+    queryset=Guest.objects.all()
+    serializer_class=GuestSerializer
+
+class ModelViewSet(viewsets.ModelViewSet):
+    queryset=Movie.objects.all()
+    serializer_class=MovieSerializer
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset=Reservation.objects.all()
+    serializer_class=ReservationSerializer
